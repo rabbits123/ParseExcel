55cc84bfd398dd9aa5db4cb86e4bb8cb08e40afe
rabbits123 <prabbit0167@gmail.com>
2018-05-18 17:07:23 +0700

    first commit

---
+++ src/parseexcel/GiangVien.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parseexcel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Phu
+ */
+public class GiangVien {
+    private String gvName;
+    
+    private List<String> feedBack;
+    private List<HashMap<String, String>> subject;
+    private int [] academicStreight;
+    private int [] present;
+    
+    public GiangVien(){
+        gvName = "";
+        feedBack = new ArrayList<>();
+        subject = new ArrayList<HashMap<String,String>>();
+        academicStreight = new int [3]; 
+        present = new int [3]; 
+    }
+
+    public void setAcademicStreight(int[] academicStreight) {
+        this.academicStreight = academicStreight;
+    }
+
+    public void setPresent(int[] present) {
+        this.present = present;
+    }
+
+    
+    public int[] getAcademicStreight() {
+        return academicStreight;
+    }
+
+    public int[] getPresent() {
+        return present;
+    }
+    
+    
+
+    public String getGvName() {
+        return gvName;
+    }
+
+    public void setGvName(String gvName) {
+        this.gvName = gvName;
+    }
+
+    public void setFeedBack(List<String> feedBack) {
+        this.feedBack = feedBack;
+    }
+
+    public List<String> getFeedBack() {
+        return feedBack;
+    }
+
+    public List<HashMap<String, String>> getSubject() {
+        return subject;
+    }
+
+    public void setSubject(List<HashMap<String, String>> subject) {
+        this.subject = subject;
+    }
+
+    
+}
---
+++ src/parseexcel/ParseExcel.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parseexcel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ *
+ * @author Phu
+ */
+public class ParseExcel {
+
+    private static Map<String, List<String>> gv_feedback = new HashMap<>();
+    private static Map<String, List<String>> gv_other = new HashMap<>();
+
+    private static List<GiangVien> gvList = new ArrayList<>();
+    
+    private static GiangVien getGV(List<GiangVien> giangVien, String name){
+        for(GiangVien gv : giangVien){
+            if (gv.getGvName().equals(name)) {
+                return gv;
+            }
+        }
+        return null;
+    }
+
+    public static void getData(String path) {
+        File src = new File(path);
+        XSSFWorkbook wb = null;
+        try {
+            FileInputStream fis = new FileInputStream(src);
+            wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet1 = wb.getSheetAt(0);
+            int end = sheet1.getLastRowNum();
+            for (int i = 0; i < end; i++) {
+                List<String> otherAttributes = new ArrayList<String>();
+
+                String GVName = sheet1.getRow(i).getCell(4).getStringCellValue();
+                String subjectCode = sheet1.getRow(i).getCell(5).getStringCellValue();
+                String subjectName = sheet1.getRow(i).getCell(6).getStringCellValue();
+                String sentence = sheet1.getRow(i).getCell(9).getStringCellValue()
+                        + sheet1.getRow(i).getCell(10).getStringCellValue();
+
+                String acaStreight = sheet1.getRow(i).getCell(7).getStringCellValue();
+                String present = sheet1.getRow(i).getCell(8).getStringCellValue();
+
+                int aca = -1;
+                switch (acaStreight) {
+                    case "Giỏi":
+                        aca = 0;
+                        break;
+                    case "Khá":
+                        aca = 1;
+                        break;
+                    case "Trung bình":
+                        aca = 2;
+                        break;
+                    case "Trung bình - Khá":
+                        aca = 3;
+                        break;
+                    case "Yếu":
+                        aca = 4;
+                        break;
+                }
+
+                int pre = -1;
+                switch (present) {
+                    case "<50%":
+                        pre = 0;
+                    case "50-80%":
+                        pre = 1;
+                    case ">80%":
+                        pre = 2;
+                }
+
+                // Nếu chưa có GV trong gvList
+                if (getGV(gvList, GVName) == null) {
+                    GiangVien gv = new GiangVien();
+                    
+                    List<String> feedback = Arrays.asList(sentence.split("\\.|\n"));
+                    Map<String, String> subject = new HashMap<>();
+                    subject.put(subjectCode, subjectName);
+                    
+                    gv.setGvName(GVName);
+                    gv.setFeedBack(feedback);
+                    gv.setSubject(subject);
+                    gv.getAcademicStreight()[aca]++;
+                    gv.getPresent()[pre]++;
+                    
+                    gvList.add(gv);
+                } else{
+                    GiangVien gv = getGV(gvList, GVName);
+                    
+                    List<String> feedback = gv.getFeedBack();
+                    feedback.addAll(Arrays.asList(sentence.split("\\.|\n")));
+                    gv.setFeedBack(feedback);
+                    
+                    // kiểm tra môn đó đã có chưa?
+                    if(!gv.getSubject().containsKey(subjectCode)){
+                        
+                    }
+                            
+                    
+                }
+
+                // preprocess --> tokenize --> preprocess
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Không tìm thấy file");
+        } catch (IOException ex) {
+            System.out.println("Lỗi");
+        } finally {
+            try {
+                wb.close();
+            } catch (IOException ex) {
+                System.out.println("Lỗi");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "Kang is the shit. he yep he is \n nah this";
+        for (String str : Arrays.asList(s.split("\\. | \n"))) {
+            System.out.println(str);
+        }
+    }
+
+}
